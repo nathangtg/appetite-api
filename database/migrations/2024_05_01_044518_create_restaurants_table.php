08196e2569f14a4fd6adf6101cdf1288b5d3f6df
nathangtg <22099865@imail.sunway.edu.my>
2024-05-01 12:52:13 +0800

    Added the restaurant table

---
+++ database/migrations/2024_05_01_044518_create_restaurants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->text('description');
+            $table->string('address');
+            $table->integer('preparation_time');
+            $table->string('cuisine');
+            $table->string('price_range');
+            $table->timestamps();
+
+            $table->primary(['id', 'admin_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
